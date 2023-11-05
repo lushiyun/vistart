acd4e49f7a72bcd70522891d067fcc24a9b321b4
Shiyun Lu <lushiyun@outlook.com>
2023-11-05 16:27:41 -0500

    chore(button): set up ui component example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import type { FunctionComponent } from "@/common/types";
-import { Button } from "@/components/ui/button";
+import Button from "@/components/ui/button";
 
 const Home = (): FunctionComponent => {
 	return (
